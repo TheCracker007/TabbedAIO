768939e42679f50ee18ccd1a48616a2ce8fd70d4
TheCracker007 <69054068+TheCracker007@users.noreply.github.com>
2023-08-29 23:13:05 +0530

    Update Top.py

--- Top.py
+++ Top.py
@@ -5,9 +5,9 @@ from tab2 import main as main_tab2
 # Set the page layout to wide mode
 st.set_page_config(layout="wide")
 
-tab = st.sidebar.radio("Tabs", ("Tab 1", "Tab 2"))
+tab = st.sidebar.radio("Content", ("Source 1", "Source 2"))
 
-if tab == "Tab 1":
+if tab == "Source 1":
     main_tab1()
-elif tab == "Tab 2":
+elif tab == "Source 2":
     main_tab2()
