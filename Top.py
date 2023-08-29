49da259d5e438708462c0036839f4495e5ff7505
TheCracker007 <69054068+TheCracker007@users.noreply.github.com>
2023-08-30 00:12:42 +0530

    Update Top.py

--- Top.py
+++ Top.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from tab1 import main as main_tab1
 from tab2 import main as main_tab2
-#from tab3 import main as main_tab3
+from tab3 import main as main_tab3
 #from tab4 import main as main_tab4
 #from tab5 import main as main_tab5
 
@@ -9,14 +9,14 @@ from tab2 import main as main_tab2
 st.set_page_config(layout="wide")
 
 #tab = st.sidebar.radio("Tabs", ("Tab 1", "Tab 2", "Tab 3", "Tab 4", "Tab 5"))
-tab = st.sidebar.radio("Content", ("Source 1", "Source 2"))
+tab = st.sidebar.radio("Content", ("Source 1", "Source 2", "Source 3"))
 
 if tab == "Source 1":
     main_tab1()
 elif tab == "Source 2":
     main_tab2()
-#elif tab == "Tab 3":
-#    main_tab3()
+elif tab == "Source 3":
+    main_tab3()
 #elif tab == "Tab 4":
 #    main_tab4()
 #elif tab == "Tab 5":
