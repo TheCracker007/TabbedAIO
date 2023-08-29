ebb46b5bc8596d58f3c10cb33187c70dec029ea3
TheCracker007 <69054068+TheCracker007@users.noreply.github.com>
2023-08-30 00:59:16 +0530

    Update tab4.py

--- tab4.py
+++ tab4.py
@@ -9,83 +9,83 @@ def main():
     # Code for second tab goes here
 
     def scrape_page(url):
-      response = requests.get(url)
-      soup = BeautifulSoup(response.content, "html.parser")
-    
-      data = []
-      table_rows = soup.select(".table-bordered tbody tr")
-    
-      for row in table_rows:
-          columns = row.find_all("td")
-          org = columns[0].text.strip()
-          last_date = columns[3].text.strip()
-          education = columns[2].text.strip()  # Extract the education column
-          job_details = columns[1].text.strip()  # Extract the job details column
-          data.append([org, last_date, education, job_details])
-    
-      return data
-    
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        data = []
+        table_rows = soup.select(".table-bordered tbody tr")
+
+        for row in table_rows:
+            columns = row.find_all("td")
+            org = columns[0].text.strip()
+            last_date = columns[3].text.strip()
+            education = columns[2].text.strip()  # Extract the education column
+            job_details = columns[1].text.strip()  # Extract the job details column
+            data.append([org, last_date, education, job_details])
+
+        return data
+
     def filter_jobs_by_education(data, education_levels):
-      filtered_data = []
-      for job in data:
-          for el in education_levels:
-              if el in job[2]:
-                  job[2] = el  # Replace the entire education string with the matched level
-                  filtered_data.append(job)
-                  break  # Stop looking for other matches once one is found
-      return filtered_data
-    
+        filtered_data = []
+        for job in data:
+            for el in education_levels:
+                if el in job[2]:
+                    job[2] = el  # Replace the entire education string with the matched level
+                    filtered_data.append(job)
+                    break  # Stop looking for other matches once one is found
+        return filtered_data
+
     def clean_job_details(data):
-      for job in data:
-          posts_info = job[3].split('-')[-1].strip()  # Extract the number of posts info
-          job[3] = posts_info  # Replace the entire job details string with the number of posts info
-      return data
-    
+        for job in data:
+            posts_info = job[3].split('-')[-1].strip()  # Extract the number of posts info
+            job[3] = posts_info  # Replace the entire job details string with the number of posts info
+        return data
+
     def scrape_all_pages(base_url, num_pages):
-      all_data = []
-      for page in range(1, num_pages + 1):
-          url = f"{base_url}/page/{page}"
-          page_data = scrape_page(url)
-          all_data.extend(page_data)
-      return all_data
-    
+        all_data = []
+        for page in range(1, num_pages + 1):
+            url = f"{base_url}/page/{page}"
+            page_data = scrape_page(url)
+            all_data.extend(page_data)
+        return all_data
+
     def parse_date(date_string):
-      try:
-          return datetime.strptime(date_string, '%d - %b - %Y')
-      except ValueError:
-          return datetime.min  # Return a very old date
-    
+        try:
+            return datetime.strptime(date_string, '%d - %b - %Y')
+        except ValueError:
+            return datetime.min  # Return a very old date
+
     def sort_by_last_date(data):
-      return sorted(data, key=lambda x: parse_date(x[1]), reverse=True)
-    
+        return sorted(data, key=lambda x: parse_date(x[1]), reverse=True)
+
     base_url = "https://allgovernmentjobs.in/latest-government-jobs"
     num_pages = 25  # Set the number of pages you want to scrape
-    
+
     all_data = scrape_all_pages(base_url, num_pages)
-    
+
     # Filter jobs by education level
     education_levels = ["B.E/ B.Tech", "Any Degree", "Electronics and Communication Engineering", "10th", "12th", "Intermediate (10+2)"]
     
     # Clean up the job details
     cleaned_data = clean_job_details(all_data)
-    
+
     sorted_data = sort_by_last_date(cleaned_data)
-    
+
     # Convert sorted_data into a DataFrame
     df_sorted_data = pd.DataFrame(sorted_data, columns=["Organisation", "Last Date", "Qualification", "Vacancies"])
-    
+
     # Display the data in a Streamlit app
     st.title('Latest Government Jobs')
-    
+
     # Add a drop-down filter for the 'Education' column with an 'Any' option
     education_option = st.selectbox(
-      'Which education level you would like to display?',
-       ['Any'] + list(df_sorted_data['Qualification'].unique()))
-    
+        'Which education level you would like to display?',
+         ['Any'] + list(df_sorted_data['Qualification'].unique()))
+
     # Filter the DataFrame based on the selected value
     if education_option == 'Any':
-      filtered_df = df_sorted_data
+        filtered_df = df_sorted_data[df_sorted_data['Qualification'].isin(education_levels)]
     else:
-      filtered_df = df_sorted_data[df_sorted_data['Qualification'] == education_option]
-    
+        filtered_df = df_sorted_data[df_sorted_data['Qualification'] == education_option]
+
     st.table(filtered_df)
