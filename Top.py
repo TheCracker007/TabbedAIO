aac3dcbfffd77ce071c3472483f5dc3bdb878147
TheCracker007 <69054068+TheCracker007@users.noreply.github.com>
2023-08-30 14:45:33 +0530

    Update Top.py

--- Top.py
+++ Top.py
@@ -6,7 +6,7 @@ from tab3 import main as main_tab3
 #from tab5 import main as main_tab5
 
 # Set the page layout to wide mode
-st.set_page_config(layout="wide")
+#st.set_page_config(layout="wide")
 
 #tab = st.sidebar.radio("Tabs", ("Tab 1", "Tab 2", "Tab 3", "Tab 4", "Tab 5"))
 #tab = st.sidebar.radio("Content", ("Source 1", "Source 2", "Source 3", "Source 4"))
