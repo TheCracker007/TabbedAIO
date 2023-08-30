9a51216ea738e8c99771502b6b72673f1e9f40fd
TheCracker007 <69054068+TheCracker007@users.noreply.github.com>
2023-08-30 14:57:51 +0530

    Update Top.py

--- Top.py
+++ Top.py
@@ -3,13 +3,13 @@ from tab1 import main as main_tab1
 from tab2 import main as main_tab2
 from tab3 import main as main_tab3
 from tab4 import main as main_tab4
-#from tab5 import main as main_tab5
 
 # Set the page layout to wide mode
 st.set_page_config(layout="wide")
 
+# ... (other parts of your script)
+
 tab = st.sidebar.radio("Content", ("Source 1", "Source 2", "Source 3", "Source 4"))
-#tab = st.sidebar.radio("Content", ("Source 1", "Source 2", "Source 3"))
 
 if tab == "Source 1":
     main_tab1()
@@ -19,5 +19,5 @@ elif tab == "Source 3":
     main_tab3()
 elif tab == "Source 4":
     main_tab4()
-#elif tab == "Tab 5":
-#    main_tab5()
+
+# ... (rest of your script)
