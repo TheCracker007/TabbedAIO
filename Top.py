b002507e72091f8553b55e5274f832e5cdaec94c
TheCracker007 <69054068+TheCracker007@users.noreply.github.com>
2023-10-05 16:31:23 +0530

    Update Top.py

--- Top.py
+++ Top.py
@@ -7,18 +7,6 @@ from tab4 import main as main_tab4
 # Set the page layout to wide mode
 st.set_page_config(layout="wide")
 
-# Your content goes here
-st.title("Streamlit App with Scroll to Bottom")
-
-# Add content to your app
-for i in range(100):
-    st.write(f"This is some content #{i+1}")
-
-# Add a "Scroll to Bottom" button
-if st.button("Scroll to Bottom"):
-    # Use JavaScript to scroll to the bottom of the page
-    st.write('<script>window.scrollTo(0, document.body.scrollHeight);</script>', unsafe_allow_html=True)
-
 tab = st.sidebar.radio("Content", ("Source 1", "Source 2", "Source 3", "Source 4"))
 
 if tab == "Source 1":
