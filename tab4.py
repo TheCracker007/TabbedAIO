cab16c379acb689ca30a4898d4c07131dfafea56
TheCracker007 <69054068+TheCracker007@users.noreply.github.com>
2023-11-29 23:13:23 +0530

    Update tab4.py

--- tab4.py
+++ tab4.py
@@ -34,8 +34,8 @@ def main():
         except ValueError:
             last_date = None
 
-        # Check if last date is not None and is on or after November 28, 2023
-        if last_date and (last_date == datetime.now().date() or last_date == datetime.now().date() - timedelta(days=1)):
+        # Check if last date is not None and is greater than or equal to yesterday's date
+        if last_date and last_date >= datetime.now() - timedelta(days=1):
             # Add the job opportunity to the list
             jobs.append({
                 'title': title,
